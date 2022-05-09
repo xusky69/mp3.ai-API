e98c71f1886b1ca7a96bf17f56c01dc31d906777
xusky69 <david.vivas@spectrio.com>
2022-05-09 12:54:10 -0500

    setup accounts app with custom user model

--- django_project/MP3AI/settings.py
+++ django_project/MP3AI/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+### PROJECT-SPECIFIC SETTINGS
+
+AUTH_USER_MODEL = 'accounts.User'
