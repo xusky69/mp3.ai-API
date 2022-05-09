c5aefb51519ea11cde2afc47922d7ae0b5f19bd0
xusky69 <david.vivas@spectrio.com>
2022-05-09 12:47:00 -0500

    setup env vars, exposing dummy django secret key just for practical purposes

--- django_project/MP3AI/settings.py
+++ django_project/MP3AI/settings.py
@@ -10,17 +10,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv('.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lwkqmal@2@b%$!&^ot)u-=21_gdf+w5&&npqt4)$r+((h7u$or'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
